bcc5e8190cd144e447e66198224bf7f9f2e6a95c
skaltman <sara.altman@posit.co>
2024-12-05 14:46:09 -0800

    remove helper script:

--- app.R
+++ app.R
@@ -10,8 +10,6 @@ library(ggplot2)
 library(dotenv)
 library(thematic)
 
-# source("helper.R")
-
 preprocess_csv <- function(csv_string) {
   csv_string <- gsub("\\\\n", "\n", csv_string)
   read.csv(text = csv_string) |> as_tibble()
--- helper.R
+++
@@ -1,4 +0,0 @@
-preprocess_csv <- function(csv_string) {
-  csv_string <- gsub("\\\\n", "\n", csv_string)
-  read.csv(text = csv_string) |> as_tibble()
-}
